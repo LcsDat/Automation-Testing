31d40c589e4b69e510d2c71a35db57fdc4599529
LcsDat <datle.testing01@gmail.com>
2025-03-24 15:55:32 +0700

    completed choosing a product type

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,11 +14,17 @@ import java.time.Duration;
 import java.util.Set;
 
 public class HomePage {
+    Actions actions;
+    WebDriverWait webDriverWait;
+
     @Test
     void testHomePage() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        actions = new Actions(driver, Duration.ofSeconds(10));
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://hasaki.vn/");
         Thread.sleep(2000);
@@ -33,10 +42,18 @@ public class HomePage {
         Assert.assertTrue(driver.findElement(By.className("item_header_hethong")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.className("item_header_giohang")).isDisplayed());
 
-        new Actions(driver).moveToElement(driver.findElement(By.className("item_login"))).perform();
+        //login
+        actions.moveToElement(driver.findElement(By.className("item_login"))).perform();
         driver.findElement(By.xpath("//a[text()='Đăng nhập' and @id='hskLoginButton']")).click();
         driver.findElement(By.cssSelector("#username")).sendKeys("0345864246");
         driver.findElement(By.cssSelector("#password")).sendKeys("#Onimusha00");
         driver.findElement(By.xpath("//button[text()='Đăng nhập']")).click();
+
+
+        actions.moveToElement(driver.findElement(By.id("hamber_menu"))).perform();
+        actions.moveToElement(driver.findElement(By.xpath("//a[@class='parent_menu' and contains(text(),'Chăm Sóc Da Mặt')]"))).perform();
+        actions.click(driver.findElement(By.xpath("//div[@class='col_hover_submenu ']//a[text()='Tẩy Trang Mặt']")))
+                .perform();
+
     }
 }
