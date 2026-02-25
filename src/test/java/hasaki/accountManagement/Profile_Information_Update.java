4521f77c9aa413d8cd59a1f7e3d9565e8af828bd
LcsDat <datle.testing01@gmail.com>
2026-02-25 19:58:58 +0700

    hello

--- src/test/java/hasaki/accountManagement/Profile_Information_Update.java
+++ src/test/java/hasaki/accountManagement/Profile_Information_Update.java
@@ -62,7 +62,7 @@ public class Profile_Information_Update extends BaseTest {
 
     @Test
     void EditProfileUnderUserAvatar(Method method) {
-        System.out.println("test again p222");
+        System.out.println("test again p2222");
         logInfo(method, "Navigate to My Account");
         homepage.navigateToMyAccount();
 
