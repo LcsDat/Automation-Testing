b2b80ac4f88bd4e1be1aebc1c6bc2b34f36bbb79
LcsDat <datle.testing01@gmail.com>
2026-02-25 09:45:21 +0700

    try trigger Jenkins again p2

--- src/test/java/hasaki/accountManagement/Profile_Information_Update.java
+++ src/test/java/hasaki/accountManagement/Profile_Information_Update.java
@@ -62,7 +62,7 @@ public class Profile_Information_Update extends BaseTest {
 
     @Test
     void EditProfileUnderUserAvatar(Method method) {
-        System.out.println("test again");
+        System.out.println("test again p2");
         logInfo(method, "Navigate to My Account");
         homepage.navigateToMyAccount();
 
